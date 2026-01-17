e7a5a8d3658032ab22c3f9cc3f090bbb0cf9db6d
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2026-01-17 12:24:10 +0500

    Sign-up router changes

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,4 +1,6 @@
 // src/app/api/sign-up/route.ts
+export const runtime = "nodejs";
+
 import { dbConnect } from "@/lib/dbConnect";
 import { UserModel } from "@/model/User.model";
 import bcrypt from "bcryptjs";
